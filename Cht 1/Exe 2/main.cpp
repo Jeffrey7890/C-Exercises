abf09597b59f42e32117aec7295f285045a80f30
Jeffrey7890 <55333325+Jeffrey7890@users.noreply.github.com>
2022-04-08 15:50:23 +0100

    Add files via upload

---
+++ Cht 1/Exe 2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "If this text,";
+    cout << " appears on your display,"
+    << endl;
+    cout <<"you can pat yourself on"
+    << " The back!" << endl;
+    return 0;
+}
